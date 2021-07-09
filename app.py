1d612d644a5a928837eef0cc36138ddbfb927cce
Naman Garg <namangarg110@gmail.com>
2021-07-09 14:18:04 +0530

    speach_to_text

--- app.py
+++ app.py
@@ -4,9 +4,27 @@ import flask
 from flask import Flask, request, render_template, redirect, url_for
 from flask_cors import CORS
 import json
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+##################### AUTH ##########################################
+apikey = 'rBzgSxh8n35Dlc7-hTC5UkzydJoXmgsEpfy7_apcGiDQ'
+url = 'https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/c2d65dda-b9cf-4270-97f8-0ac9c7af013a'
+#####################################################################
+
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route('/<path>')
+def speach_to_text(path):
+    authenticator = IAMAuthenticator(apikey)
+    stt = SpeechToTextV1(authenticator=authenticator)
+    stt.set_service_url(url)
+    with open(path, 'rb') as f:
+        res = stt.recognize(audio=f, content_type='audio/mp3', model='en-US_NarrowbandModel', continuous=True).get_result()
+    return res
 
 
 @app.route('/<og_img_path>/<res_img_path>/<width>/<height>')
